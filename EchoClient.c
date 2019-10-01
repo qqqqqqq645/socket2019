98993058c1514e3b612667ff462b6af016ac70a6
qqqqqq645 <dregdog1234@gmail.com>
2019-10-01 20:50:22 +0900

    오탈자 수정

--- EchoClient.c
+++ EchoClient.c
@@ -55,7 +55,7 @@ int main(){
 		}
 		rcvBuffer[n] = '\0'; //문자열 뒷부분 깨짐 방지
 		printf("received data: %s\n", rcvBuffer); //서버에서 받은 메세지 출력
-		printf("rcvBuffer length: %d\n", n); //3-2. 서버에섭 다은 메세지의 길이 출력 
+		printf("rcvBuffer length: %d\n", n); //3-2. 서버에섭 받은 메세지의 길이 출력 
 	}
 	close(c_socket);
 	return 0;	
--- EchoServer.c
+++ EchoServer.c
@@ -53,11 +53,13 @@ int main(){
 		while(1){
 			n = read(c_socket, rcvBuffer, sizeof(rcvBuffer));
 			printf("rcvBuffer = %s\n",rcvBuffer);
+		//클라이언트가 quit또는 kill server 입력시 클라이언트 소켓 종료
 			if(strncasecmp(rcvBuffer,"quit",4)==0 ||strncasecmp(rcvBuffer,"kill server",11)==0 )
 				break;
 			write(c_socket, buffer, n); //클라이언트에게 buffer의 내용을 전송함
 		}
 		close(c_socket);
+		//kill server 입력시 서버소켓도 종료
 		if(strncasecmp(rcvBuffer,"kill server",11)==0)
 		break;
 	}
