b5276b270cd41a468e29d4081377e81553372b41
qqqqqq645 <dregdog1234@gmail.com>
2019-12-01 23:13:18 +0900

    귓속말 기능

--- MultiThreadChat/MultiThreadChatClient.c
+++ MultiThreadChat/MultiThreadChatClient.c
@@ -9,7 +9,7 @@
 #include <signal.h>
 #define CHATDATA 1024
 #define IPADDR "127.0.0.1"
-#define PORT 9063
+#define PORT 9980
 void *do_send_chat(void *);
 void *do_receive_chat(void *);
 pthread_t thread_1, thread_2;
--- MultiThreadChat/MultiThreadChatServer.c
+++ MultiThreadChat/MultiThreadChatServer.c
@@ -8,13 +8,13 @@
 void *do_chat(void *); //채팅 메세지를 보내는 함수
 int pushClient(int); //새로운 클라이언트가 접속했을 때 클라이언트 정보 추가
 int popClient(int); //클라이언트가 종료했을 때 클라이언트 정보 삭제
-int c_stack; //클라이언트 스택 관리
+int top; //클라이언트 스택 관리
 pthread_t thread;
 pthread_mutex_t mutex;
-#define MAX_CLIENT 3 
+#define MAX_CLIENT 4
 #define CHATDATA 1024
 #define INVALID_SOCK -1
-#define PORT 9063
+#define PORT 9980
 //int    list_c[MAX_CLIENT];
 typedef struct __clientData{
 	int c_socket;
@@ -61,28 +61,26 @@ int main(int argc, char *argv[ ])
             write(c_socket, greeting, strlen(greeting));
             //pthread_create with do_chat function.
             //스레드 생선
-            int status = pthread_create(&thread,NULL,do_chat,(void *)&c_socket);
+            //int status = pthread_create(&thread,NULL,do_chat,(void *)&c_socket);
+            int status = pthread_create(&thread,NULL,do_chat,(void *)&list_c[res]);
         }
-		printf("c_stack = %d\n",c_stack);
+		printf("top = %d\n",top);
     }
 }
 void *do_chat(void *arg)
 {
-    int c_socket = *((int *)arg);
+    //int c_socket = *((int *)arg);
+	int c_socket = *((int *)arg); //클라이언트 socket 정보 저장
+	//int c_socket = ((clientData *)arg->c_socket);
+	printf("c_socket add = %d\n",(int *) arg);
+	char username [20];
+	strcpy(username,(char *)(arg+sizeof(int))); //nickname 저장
     char chatData[CHATDATA];
-	char *name;
     int i=0, n;
-	while(i<MAX_CLIENT){
-		if(list_c[i].c_socket == c_socket){
-			name = list_c[i].nickname;
-			break;
-		}
-		i++;
-	}
-	printf("now name = %s\n",name);
+	printf("now name = %s\n",username);
     while(1) {
-        memset(chatData, 0, sizeof(chatData));
-		printf("%s c_stack = %d\n",name,c_stack);
+        memset(chatData, 0, sizeof(chatData)); 
+		printf("%s top = %d\n",username,top);
         if((n = read(c_socket, chatData, sizeof(chatData))) > 0) {
             //write chatData to all clients
 			printf("data = %s\n",chatData);
@@ -91,36 +89,47 @@ void *do_chat(void *arg)
            	 break;
         	}
 			char *usrMsg = chatData;
-			usrMsg = usrMsg+(strlen(name)+3);//"[닉네임] " 만큼 문자
-			if(strncasecmp(usrMsg,"/w",2)==0){
-				char namedesc[20];
+			usrMsg = usrMsg+(strlen(username)+3);//"[닉네임] " 만큼 문자열 포인터 이동
+			if(strncasecmp(usrMsg,"/who",4)==0){
+				i = 0;
+					memset(chatData,0,sizeof(chatData));
+					sprintf(chatData,"There are %d people in chatroom\n",top);
+				while(i<top){
+					strcat(chatData,list_c[i].nickname);
+					if(strcmp(username,list_c[i].nickname)==0)
+						strcat(chatData," (You)");
+					strcat(chatData,"\n");
+					i++;
+				}
+				write(c_socket,chatData,strlen(chatData));
+			}
+			else if(strncasecmp(usrMsg,"/w",2)==0){ //귓속말 판별
+				char namedesc[20]; //귓속말 대상 닉네임
 				char *msg;
 				char sendMsg[CHATDATA];
-				memset(sendMsg,0,sizeof(sendMsg));
+				memset(sendMsg,0,sizeof(sendMsg)); //보낼 메세지 문자열 초기화
 				strtok(usrMsg," ");// "/w"
 				printf("usrMsg = %s\n",usrMsg);
-				msg = strtok(NULL," ");
+				msg = strtok(NULL," ");//닉네임 토큰
 				if(msg != NULL){
-					strcpy(namedesc,msg);
-					printf("지금");
-					printf("namedesc = %s\n",namedesc);
-					msg = strtok(NULL," ");
-					while(msg !=NULL){
+					strcpy(namedesc,msg); //대상 닉네임 복사
+					msg = strtok(NULL," ");//다음 토큰으로 이동
+					while(msg !=NULL){ //메세지 재조립 
 						strcat(sendMsg,msg);
 						strcat(sendMsg," ");
-						msg = strtok(NULL," ");
+						msg = strtok(NULL," "); 
 					}
 					i=0;
-					while(i<MAX_CLIENT){
+					while(i<top){//0부터 top까지 해당 닉네임이 있는지 탐색
 						if(strncasecmp(namedesc, list_c[i].nickname, strlen(namedesc)) == 0){
-							sprintf(chatData,"[%s]'s whispher : %s\n",name,sendMsg);
+							sprintf(chatData,"[%s]'s whispher : %s",username,sendMsg);
 							write(list_c[i].c_socket,chatData,strlen(chatData));
 							break;	
 						}
 						else 
 							i++;
 					}
-					if(i == MAX_CLIENT){
+					if(i == top){ //top까지 탐색했는데 없을 경우
 						sprintf(chatData,"no such user\n");
 						write(c_socket,chatData,strlen(chatData));
 					}
@@ -132,8 +141,8 @@ void *do_chat(void *arg)
 				}
 					
 			}
-			
-			else{	
+			//else if (strncasecmp(usrMsg,"/
+			else { // 아무 명령어 없을 경우 전체 대화
 				i=0;
 				while(i<MAX_CLIENT){
 					write(list_c[i].c_socket,chatData,strlen(chatData));
@@ -147,17 +156,17 @@ void *do_chat(void *arg)
 
 int pushClient(int c_socket) {
     //ADD c_socket to list_c array.
-    //
-    ///////////////////////////////
 	int i=0;
 	pthread_mutex_lock(&mutex);
-	if(c_stack<MAX_CLIENT){
-			list_c[c_stack].c_socket = c_socket;
-			read(c_socket,list_c[c_stack].nickname,sizeof(sizeof(char)*20));//receive nickname from client
-			//c_stack++;
+	if(top<MAX_CLIENT){
+			list_c[top].c_socket = c_socket;
+			read(c_socket,list_c[top].nickname,sizeof(sizeof(char)*20));//receive nickname from client
 			pthread_mutex_unlock(&mutex);
-			printf("c_stack in pushC = %d\n",c_stack);
-			return c_stack++;
+			printf("top in pushC = %d\n",top);
+			printf("list add[toop] = %d\n", (int *)&list_c[top]);
+			printf("sockadd = %d\n",(int *)&list_c[top].c_socket);
+			printf("nameadd = %d\n",(int *)&list_c[top].nickname);
+			return top++;
 		} 
 	else
 	
@@ -171,21 +180,11 @@ int popClient(int c_socket)
     close(c_socket);
 	int i=0;
     //REMOVE c_socket from list_c array.
-	pthread_mutex_lock(&mutex);/*
-	while(i<c_stack-1){
-		if(list_c[i].c_socket == c_socket){
-			list_c[i].c_socket = list_c[i+1].c_socket;
-			//memset((char *)&list_c[i].nickname,'\0',sizeof(char)*20);
-			strcpy(list_c[i].nickname,list_c[i+1].nickname);
-	//		pthread_mutex_unlock(&mutex);
-	//		return i;
-		}
-			i++;
-	}*/
-	while(i<c_stack-1){
+	pthread_mutex_lock(&mutex);
+	while(i<top-1){
 		if(list_c[i].c_socket == c_socket){
 			int j = i;
-			while(j<c_stack-1){
+			while(j<top-1){
 				list_c[j].c_socket = list_c[j+1].c_socket;
 				strcpy(list_c[j].nickname,list_c[j+1].nickname);
 				j++;
@@ -194,15 +193,13 @@ int popClient(int c_socket)
 		}
 		i++;
 	}
-	//list_c[i+1].c_socket = INVALID_SOCK;
-	//memset(list_c[i+1].nickname,0,strlen(list_c[i+1].nickname));
-			printf("c_stack in popC before before mutex = %d\n",c_stack);
-	list_c[--c_stack].c_socket = INVALID_SOCK;
-	memset(list_c[c_stack].nickname,0,strlen(list_c[c_stack].nickname));
-			printf("c_stack in popC before mutex = %d\n",c_stack);
+			printf("top in popC before before mutex = %d\n",top);
+	list_c[--top].c_socket = INVALID_SOCK;
+	memset(list_c[top].nickname,0,strlen(list_c[top].nickname));
+			printf("top in popC before mutex = %d\n",top);
 	pthread_mutex_unlock(&mutex);
-			printf("c_stack in popC = %d\n",c_stack);
-	return c_stack;
+			printf("top in popC = %d\n",top);
+	return top;
 	
 
 }
